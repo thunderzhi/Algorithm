4d061b3829bf0e4dc395e23962c03fd32e1a8a69
thunderzhi <465785021@qq.com>
2020-08-06 20:00:10 +0800

    new sort

---
+++ src/main/java/org/cxz/algorithm/sort/SelectionSortDemo.java
@@ -0,0 +1,25 @@
+package org.cxz.algorithm.sort;
+
+import java.util.Arrays;
+
+/**
+ * @author cxz
+ * @Title:
+ * @Package
+ * @Description:
+ * @date 2020/8/6 19:43
+ */
+public class SelectionSortDemo {
+    public static void main(String[] args) {
+        int[] arr =new int[]{3,44,38,5,47,15,36,26,27,2,46,4,19,50,48};
+        SelectionSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    public static void SelectionSort(int[] a){
+
+
+
+
+
+    }
+}
