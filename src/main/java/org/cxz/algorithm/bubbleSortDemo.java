d1c83b642b32a2382d8d257c07f91fff36170c96
thunderzhi <465785021@qq.com>
2020-08-06 17:54:51 +0800

    insertsort

--- src/main/java/org/cxz/algorithm/bubbleSortDemo.java
+++ src/main/java/org/cxz/algorithm/bubbleSortDemo.java
@@ -1,11 +1,45 @@
 package org.cxz.algorithm;
 
+import java.util.Arrays;
+import java.util.concurrent.ForkJoinPool;
+
 public class bubbleSortDemo {
     public static void main(String[] args) {
+      int[] array = new int[]{20, 40, 60, 80, 30, 70, 90, 10, 50, 0};
 
+        //BubbleSort(array);
+        BubbleSort2(array);
+        System.out.println("end "+Arrays.toString(array));
     }
 
     public static void BubbleSort(int[] arr){
+        boolean flag= false;
+        for (int i = (arr.length-1); i >0 ; i--) {
 
+            System.out.println("i "+i+" "+Arrays.toString(arr));
+            for (int j = 0; j <i ; j++) {
+                if (arr[j]>arr[j+1]){
+                    //swap
+                    int tmp = arr[j+1];
+                    arr[j+1]=arr[j];
+                    arr[j]=tmp;
+                }
+            }
+        }
+    }
+    public static void BubbleSort2(int[] arr){
+        for(int i =0 ; i<arr.length-1 ; i++) {
+            System.out.println("i "+i+" "+Arrays.toString(arr));
+            for(int j=0 ; j<arr.length-1-i ; j++) {
+
+                if(arr[j]>arr[j+1]) {
+                    int temp = arr[j];
+
+                    arr[j]=arr[j+1];
+
+                    arr[j+1]=temp;
+                }
+            }
+        }
     }
 }
--- src/main/java/org/cxz/algorithm/insertionSortDemo.java
+++ src/main/java/org/cxz/algorithm/insertionSortDemo.java
@@ -1,4 +1,27 @@
 package org.cxz.algorithm;
 
+import java.util.Arrays;
+
 public class insertionSortDemo {
+    public static void main(String[] args) {
+        int[] array = new int[]{20, 40, 60, 80, 30, 70, 90, 10, 50, 0};
+        InsertionSort(array);
+        System.out.println("end "+ Arrays.toString(array));
+    }
+
+    public static void InsertionSort(int[] arr){
+        int length = arr.length;
+        for (int i =0 ; i <length-1; i++) {
+
+            System.out.println("i "+i+" "+Arrays.toString(arr));
+            for (int j =i+1; j>0&&arr[j]<arr[j-1] ; j--) {
+                int tmp =0;
+                if (arr[j-1]>arr[j]){
+                    tmp = arr[j];
+                    arr[j]=arr[j-1];
+                    arr[j-1] =tmp;
+                }
+            }
+        }
+    }
 }
