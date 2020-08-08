8be575a0228a7e5d6381bf491dcb4e28a36940fd
thunderzhi <465785021@qq.com>
2020-08-08 19:40:11 +0800

    somethiing wrong

--- src/main/java/org/cxz/algorithm/sort/QuickSortDemo.java
+++ src/main/java/org/cxz/algorithm/sort/QuickSortDemo.java
@@ -12,41 +12,60 @@ import java.util.Arrays;
 public class QuickSortDemo {
     public static void main(String[] args) {
         int[] arr =new int[]{3,44,38,5,47,15,36,26,27,2,46,4,19,50,48};
-
+        System.out.println("arr length is"+ arr.length);
         QuickSort(arr,0,arr.length-1);
         System.out.println(Arrays.toString(arr));
     }
 
     public static void QuickSort(int[] a,int start ,int end){
+        if (end>start){
+            int i =start,j= end;
+            int pivot = 0;
+            pivot = partition(a,start,end);
+            System.out.println("****************strat= "+start+" **********end= "+end+"*****************************************");
+            System.out.println("pivot index = "+pivot+" i= "+i+" j= "+j+" "+Arrays.toString(a));
 
-        int i =start,j= end;
-        int pivot = a[i];
-        while (i<j){
-            while (i<j&&a[j]>pivot){
-                j--;
-            }
-            while (i<j&&a[i]<pivot){
-                i++;
-            }
-            if (a[j]==a[i]&&i<j){
-                i++;
-            }else{
-                int tmp = a[j];
-                a[j]=a[i];
-                a[i]=tmp;
-            }
-            System.out.println("i= "+i+"j="+j+" "+Arrays.toString(a));
-
-            if (start<i-1) {
-                QuickSort(a,start,i-1);
-            }
-
-            if (j+1<end) {
-                QuickSort(a,j+1,end);
-            }
+            QuickSort(a,start,pivot-1);
+            QuickSort(a,pivot+1,end);
         }
 
     }
 
+    public static int partition(int[] a,int low  ,int high ){
+        System.out.println("low = "+low +" high = "+high);
 
+        int key =a[low];
+        System.out.println("key value is  a["+low+"]=  "+key );
+        int pivotindex =low;
+        //low = low+1;
+        int index =0;
+        while (low<high){
+            while (low<high&& a[high]>key){
+                high--;
+            }
+            while (low<high&&a[low]<key){
+                low++;
+            }
+            System.out.println(" high = "+high+" --->"+a[high]+" low= "+ low+" ----->"+a[low] );
+            if (low<high){
+                int tmp = a[low];
+                a[low]=a[high];
+                a[high]=tmp;
+                System.out.println(" swaped ==>   "+a[high]+" <---> "+ a[low] );
+            }
+            if (low==high&&a[pivotindex]>a[low]){
+
+                a[pivotindex]=a[low];
+                a[low] =key;
+                System.out.println("key ------> a[l]="+a[pivotindex]);
+            }
+            index =low;
+            System.out.println("partition key = "+key+" low= "+low+" high= "+high+" "+Arrays.toString(a));
+        }
+        if (index==0){
+
+        }
+
+        return index;
+    }
 }
