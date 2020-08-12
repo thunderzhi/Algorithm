0275196f4dc5787461604d2b189bbe4f7de8d5fb
cxz39223 <cxz39223@ly.com>
2020-08-12 20:59:26 +0800

    some function

--- src/main/java/org/cxz/DataStructures/tree/Tree.java
+++ src/main/java/org/cxz/DataStructures/tree/Tree.java
@@ -10,13 +10,127 @@ package org.cxz.DataStructures.tree;
 public class Tree {
     private Node root;             // first node of tree
 
-    public void find(int key){
-
-    }
+    public Node find(int key)      // find node with given key
+    {                           // (assumes non-empty tree)
+        Node current = root;               // start at root
+        while(current.iData != key)        // while no match,
+        {
+            if(key < current.iData)         // go left?
+                current = current.leftChild;
+            else                            // or go right?
+                current = current.rightChild;
+            if(current == null)             // if no child,
+                return null;                 // didn't find it
+        }
+        return current;                    // found it
+    }  // end find()
     public void insert(int id, double dd){
+        Node newNode = new Node();    // make new node
+        newNode.iData = id;           // insert data
+        newNode.dData = dd;
+        if(root==null)                // no node in root
+            root = newNode;
+        else                          // root occupied
+        {
+            Node current = root;       // start at root
+            Node parent;
+            while(true)                // (exits internally)
+            {
+                parent = current;
+                if(id < current.iData)  // go left?
+                {
+                    current = current.leftChild;
+                    if(current == null)  // if end of the line,
+                    {                 // insert on left
+                        parent.leftChild = newNode;
+                        return;
+                    }
+                }  // end if go left
+                else                    // or go right?
+                {
+                    current = current.rightChild;
+                    if(current == null)  // if end of the line
+                    {                 // insert on right
+                        parent.rightChild = newNode;
+                        return;
+                    }
+                }  // end else go right
+            }  // end while
+        }  // end else not root
+    }  // end insert()
+    public boolean delete(int key){
+        Node current = root;
+        Node parent = root;
+        boolean isLeftChild = true;
 
-    }
-    public void delete(int id){
+        while(current.iData != key)        // search for node
+        {
+            parent = current;
+            if(key < current.iData)         // go left?
+            {
+                isLeftChild = true;
+                current = current.leftChild;
+            }
+            else                            // or go right?
+            {
+                isLeftChild = false;
+                current = current.rightChild;
+            }
+            if(current == null)             // end of the line,
+            {
+                return false;                // didn't find it
+            }
+        }  // end while
+        // found node to delete
 
+        // if no children, simply delete it
+        if(current.leftChild==null &&
+                current.rightChild==null)
+        {
+            if(current == root)             // if root,
+                root = null;                 // tree is empty
+            else if(isLeftChild)
+                parent.leftChild = null;     // disconnect
+            else                            // from parent
+                parent.rightChild = null;
+        }
+
+        // if no right child, replace with left subtree
+        else if(current.rightChild==null)
+            if(current == root)
+                root = current.leftChild;
+            else if(isLeftChild)
+                parent.leftChild = current.leftChild;
+            else
+                parent.rightChild = current.leftChild;
+
+            // if no left child, replace with right subtree
+        else if(current.leftChild==null)
+            if(current == root)
+                root = current.rightChild;
+            else if(isLeftChild)
+                parent.leftChild = current.rightChild;
+            else
+                parent.rightChild = current.rightChild;
+
+        else  // two children, so replace with inorder successor
+        {
+            // get successor of node to delete (current)
+            Node successor = getSuccessor(current);
+
+            // connect parent of current to successor instead
+            if(current == root)
+                root = successor;
+            else if(isLeftChild)
+                parent.leftChild = successor;
+            else
+                parent.rightChild = successor;
+
+            // connect successor to current's left child
+            successor.leftChild = current.leftChild;
+        }  // end else two children
+        // (successor cannot have a left child)
+        return true;
     }
+
 }
