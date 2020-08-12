b5cf21afdb9bf538d9b25e56012d29b5b03be958
thunderzhi <465785021@qq.com>
2020-08-12 14:27:04 +0800

    new file

---
+++ src/main/java/org/cxz/DataStructures/tree/Tree.java
@@ -0,0 +1,22 @@
+package org.cxz.DataStructures.tree;
+
+/**
+ * @author cxz
+ * @Title:
+ * @Package
+ * @Description:
+ * @date 2020/8/11 16:45
+ */
+public class Tree {
+    private Node root;             // first node of tree
+
+    public void find(int key){
+
+    }
+    public void insert(int id, double dd){
+
+    }
+    public void delete(int id){
+
+    }
+}
