ff78a98f36d36ba0f5162d6302548ecacf32f568
Eric.Cui <465785021@qq.com>
2021-03-14 15:17:15 +0800

    test

---
+++ src/main/java/org/cxz/algorithm/linkedlist/case86.java
@@ -0,0 +1,19 @@
+package org.cxz.algorithm.linkedlist;
+
+/**
+ * @author cxz
+ * @Title:
+ * @Package
+ * @Description:
+ * @date 2021/3/13 20:35
+ */
+public class case86 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static ListNode partition(ListNode head, int x) {
+
+    }
+}
