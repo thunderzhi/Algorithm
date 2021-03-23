903ff55e8bb67d7c94e4c2b458440a960fcb08c0
Eric.Cui <465785021@qq.com>
2021-03-23 15:35:14 +0800

    227,331,636

--- src/main/java/org/cxz/algorithm/stack/case227.java
+++ src/main/java/org/cxz/algorithm/stack/case227.java
@@ -18,27 +18,53 @@ public class case227 {
          String s = "3+277*2";
          new case227().calculate(s);
     }
-    public int calculate(String s) {
-        Stack<String> op = new Stack<>();
-        Stack<Integer> num = new Stack<Integer>();
-        char[] operator = {'+','-','*','/'};
-        String[] arr = s.split("[+ - * /]");
-        char[] chars = s.toCharArray();
-        LinkedList<String> linkedList = new LinkedList<String>();
-        for (int i = 0; i <chars.length ; i++) {
-            String tmp = String.valueOf(chars[i]);
-            linkedList.offer(tmp);
+    public int level(char c){
+        switch (c){
+            case '@':return -1;
+            case '+':
+            case '-':return 1;
+            case '*':
+            case '/':return 2;
         }
-
-        for (int i = 0; i < arr.length; i++) {
-
+        return 0;
+    }
+    public int calc(char op,int a,int b){
+        switch (op){
+            case '+': return a+b;
+            case '-': return a-b;
+            case '*': return a*b;
+            case '/': return a/b;
         }
-        
-
-
-
-        return  0;
+        return 0;
     }
 
+    public int calculate(String s) {
+        Stack<Character> op = new Stack<>();
+        Stack<Integer> num = new Stack<Integer>();
+        s += "@";
+
+        int n = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == ' ') {
+                continue;
+            }
+            if (level(s.charAt(i)) == 0) {
+                n = n * 10 + (s.charAt(i) - '0');
+                continue;
+            }
+            num.push(n);
+            n = 0;
+            while (!op.isEmpty() && level(s.charAt(i)) <= level(op.peek())) {
+                int b = num.peek();
+                num.pop();
+                int a = num.peek();
+                num.pop();
+                num.push(calc(op.peek(), a, b));
+                op.pop();
+            }
+            op.push(s.charAt(i));
+        }
+        return num.peek();
+    }
 
 }
--- src/main/java/org/cxz/algorithm/stack/case331.java
+++ src/main/java/org/cxz/algorithm/stack/case331.java
@@ -13,26 +13,24 @@ public class case331 {
     public static void main(String[] args) {
         String s ="9,3,4,#,#,1,#,#,2,#,6,#,#";
         boolean validSerialization = new case331().isValidSerialization(s);
-
+        System.out.println(validSerialization);
     }
     public boolean isValidSerialization(String preorder) {
         Stack<String> s = new Stack<>();
         String[] split =preorder.split(",");
-        for (int i = 0; i <split.length ; i++) {
+        for (int i = 0 ,j = 0  ; i <split.length ; i++) {
             s.push(split[i]);
+            int last = s.size()-1;
+            while(s.size()>=3&&s.get(last).equals("#")
+                    &&s.get(last-1).equals("#")
+                    &&!s.get(last-2).equals("#")){
+                s.set(last-2,"#");
+                s.pop();
+                s.pop();
+                last = s.size()-1;
+            }
         }
-
-        int last = s.size()-1;
-        while (s.size()>=3&&s.get(last).equals("#")
-                &&s.get(last-1).equals("#")
-                &&!s.get(last-2).equals("#")
-        ){
-            s.set(last-2,"#");
-            s.pop();
-            s.pop();
-            last = s.size()-1;
-        }
-      
         return s.size()==1&&s.get(0).equals("#");
+
     }
 }
