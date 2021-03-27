36a522307083ff7b31b7c9749c4e193847a8ca40
Eric.Cui <465785021@qq.com>
2021-03-27 10:03:16 +0800

    pass

--- src/main/java/org/cxz/algorithm/linkedlist/case0204.java
+++ src/main/java/org/cxz/algorithm/linkedlist/case0204.java
@@ -9,6 +9,32 @@ package org.cxz.algorithm.linkedlist;
  */
 public class case0204 {
     public ListNode partition(ListNode head, int x) {
+        if (head==null){
+            return null;
+        }
+
+        ListNode p = head;
+        ListNode l1 = new ListNode(0);;
+        ListNode l2 = new ListNode(0);;
+        ListNode l1end = l1;
+        ListNode l2end = l2;
+
+        while(p!=null){
+            if(p.val<x){
+                //l1
+                l1end.next = p;
+                l1end = l1end.next;
+            }
+            else{
+                //l2
+                l2end.next = p;
+                l2end = l2end.next;
+            }
+            p= p.next;
+        }
+        l2end.next = null;
+        l1end.next = l2.next;
+        return l1.next;
 
     }
 }
