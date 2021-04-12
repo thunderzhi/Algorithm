304983706e9a0d002224ca579db94625ceaff885
Eric.Cui <465785021@qq.com>
2021-04-12 19:25:31 +0800

    n tree

---
+++ src/main/java/org/cxz/algorithm/heap/case1046.java
@@ -0,0 +1,8 @@
+package org.cxz.algorithm.heap;/**  
+* @Title: 
+* @Package 
+* @Description: 
+* @author cxz
+* @date 2021/4/10 16:57
+*/public class case1046 {
+}
---
+++ src/main/java/org/cxz/algorithm/treepack/case589.java
@@ -0,0 +1,31 @@
+package org.cxz.algorithm.treepack;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author cxz
+ * @Title:
+ * @Package
+ * @Description:
+ * @date 2021/4/12 19:17
+ */
+public class case589{
+
+//    void _preorder(Node root,List<Integer> ans){
+//        if(root == null){
+//            return;
+//        }
+//        ans.add(root.val);
+//        for (int i = 0; i <root.children.size() ; i++) {
+//            _preorder(root.children.get(i),ans);
+//        }
+//        return;
+//    }
+//
+//    public List<Integer> preorder(Node root) {
+//        List<Integer> ans = new ArrayList<>();
+//        _preorder(root,ans);
+//        return ans;
+//    }
+}
