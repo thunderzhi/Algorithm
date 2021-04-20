9dc5b5ba8f1dab5ae442752a8f9602124155d5ea
Eric.Cui <465785021@qq.com>
2021-04-20 23:00:45 +0800

    frequencySort

--- src/main/java/org/cxz/algorithm/treepack/case451.java
+++ src/main/java/org/cxz/algorithm/treepack/case451.java
@@ -1,5 +1,10 @@
 package org.cxz.algorithm.treepack;
 
+import java.awt.color.ICC_ProfileGray;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.PriorityQueue;
+
 /**
  * @author cxz
  * @Title:
@@ -9,5 +14,32 @@ package org.cxz.algorithm.treepack;
  */
 public class case451 {
 
+    public String frequencySort(String s) {
+        char[] chars = s.toCharArray();
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < chars.length; i++) {
+            map.put(chars[i],map.getOrDefault(chars[i],0)+1);
+        }
+        PriorityQueue<Character> pq = new PriorityQueue<>(new Comparator<Character>() {
+            @Override
+            public int compare(Character o1, Character o2) {
+                return map.get(o2) - map.get(o1);
+            }
+        });
+        for (Character character : map.keySet()) {
 
+            pq.offer(character);
+        }
+        StringBuilder sb = new StringBuilder();
+        while(!pq.isEmpty()){
+            Character poll = pq.poll();
+            int leng = map.get(poll);
+            for (int i = 0; i < leng; i++) {
+                sb.append(poll);
+            }
+        }
+        return sb.toString();
+
+
+    }
 }
