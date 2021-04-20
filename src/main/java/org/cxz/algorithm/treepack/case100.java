dad8481667bfd55a8e73c1a2a98191e05d4c038b
Eric.Cui <465785021@qq.com>
2021-04-20 10:28:29 +0800

    isSameTree

--- src/main/java/org/cxz/algorithm/treepack/case100.java
+++ src/main/java/org/cxz/algorithm/treepack/case100.java
@@ -1,8 +1,26 @@
-package org.cxz.algorithm.treepack;/**  
-* @Title: 
-* @Package 
-* @Description: 
-* @author cxz
-* @date 2021/4/20 9:43
-*/public class case100 {
+package org.cxz.algorithm.treepack;
+
+/**
+ * @author cxz
+ * @Title:
+ * @Package
+ * @Description:
+ * @date 2021/4/20 9:43
+ */
+public class case100 {
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p==null&&q!=null){
+            return false;
+        }
+        if(p!=null&&q==null){
+            return false;
+        }
+        if(p==null&&q==null){
+            return true ;
+        }
+
+        return p.val==q.val&&isSameTree(p.left,q.left)&&isSameTree(p.right,q.right);
+    }
+ 
 }
