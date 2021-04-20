8726e552ad289d15ea44bb028b47115c98caa6f5
Eric.Cui <465785021@qq.com>
2021-04-20 14:58:11 +0800

    _isSymmetric

--- src/main/java/org/cxz/algorithm/treepack/case101.java
+++ src/main/java/org/cxz/algorithm/treepack/case101.java
@@ -1,5 +1,13 @@
 package org.cxz.algorithm.treepack;
 
+import org.cxz.DataStructures.tree.Tree;
+
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 /**
  * @author cxz
  * @Title:
@@ -11,5 +19,29 @@ public class case101 {
 
     public boolean isSymmetric(TreeNode root) {
 
+        if(root==null){
+            return true;
+        }
+        if(root!=null&&root.left==null&&root.right==null){
+            return true;
+        }
+
+        return _isSymmetric(root.left,root.right);
     }
+
+    public boolean _isSymmetric(TreeNode a,TreeNode b){
+        if(a==null&&b==null){
+             return true;
+        }
+        if(a==null||b==null){
+            return false;
+        }
+        if(a.val!=b.val){
+            return false;
+        }
+
+        return _isSymmetric(a.right,b.left)&&_isSymmetric(a.left,b.right);
+
+    }
+
 }
