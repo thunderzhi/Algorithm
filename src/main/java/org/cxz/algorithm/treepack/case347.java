f5f0ecef702e4f6cb02c78e5f7a3d65564a8ea6e
Eric.Cui <465785021@qq.com>
2021-04-20 21:49:52 +0800

    PriorityQueue map

--- src/main/java/org/cxz/algorithm/treepack/case347.java
+++ src/main/java/org/cxz/algorithm/treepack/case347.java
@@ -13,46 +13,29 @@ import java.util.PriorityQueue;
  * @date 2021/4/20 21:31
  */
 public class case347 {
-    public class TwoTuple<A,B> {
-        public final A first;
-
-        public final B second;
-
-        public TwoTuple(A a, B b){
-            first = a;
-            second = b;
-        }
-    }
-
 
     public int[] topKFrequent(int[] nums, int k) {
-
         Map<Integer, Integer> map = new HashMap<>();
-
         for (int i = 0; i < nums.length; i++) {
             map.put(nums[i],map.getOrDefault(nums[i],0)+1);
         }
-
-        PriorityQueue<TwoTuple<Integer, Integer>> pq = new PriorityQueue<>(new Comparator<TwoTuple<Integer, Integer>>() {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(new Comparator<Integer>() {
             @Override
-            public int compare(TwoTuple<Integer, Integer> o1, TwoTuple<Integer, Integer> o2) {
-                return o1.second - o2.second;
+            public int compare(Integer o1, Integer o2) {
+                return map.get(o1) - map.get(o2);
             }
         });
         for (Integer integer : map.keySet()) {
-            TwoTuple<Integer, Integer> tw = new TwoTuple<Integer, Integer>(integer,map.get(integer));
-            pq.offer(tw);
+
+            pq.offer(integer);
             if(pq.size()>k){
                 pq.poll();
             }
         }
         int[] ans = new int[k];
         for (int i = 0; i < ans.length; i++) {
-            ans[i] = pq.poll().first;
+            ans[i] = pq.poll();
         }
-
         return ans;
-
-
     }
 }
