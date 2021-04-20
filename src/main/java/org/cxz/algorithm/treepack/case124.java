fdf7bd00f1c536b44cf85ca217e2bcda4d50321f
Eric.Cui <465785021@qq.com>
2021-04-20 21:26:37 +0800

    maxPathSum

--- src/main/java/org/cxz/algorithm/treepack/case124.java
+++ src/main/java/org/cxz/algorithm/treepack/case124.java
@@ -8,8 +8,19 @@ package org.cxz.algorithm.treepack;
  * @date 2021/4/20 16:37
  */
 public class case124 {
-
+    public int sum  = Integer.MIN_VALUE;
     public int maxPathSum(TreeNode root) {
+        getmax(root);
+        return sum;
+    }
 
+    public int getmax(TreeNode root){
+        if(root == null){
+            return 0;
+        }
+        int l = Math.max(getmax(root.left),0);
+        int r = Math.max(getmax(root.right),0);
+        sum = Math.max(sum, root.val+l+r);
+        return root.val+ Math.max(l,r);
     }
 }
