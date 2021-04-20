15ff447797778f153bf52730fc6df014093b46b9
Eric.Cui <465785021@qq.com>
2021-04-20 21:53:14 +0800

    case973

---
+++ src/main/java/org/cxz/algorithm/treepack/case973.java
@@ -0,0 +1,15 @@
+package org.cxz.algorithm.treepack;
+
+/**
+ * @author cxz
+ * @Title:
+ * @Package
+ * @Description:
+ * @date 2021/4/20 21:52
+ */
+public class case973 {
+
+    public int[][] kClosest(int[][] points, int k) {
+
+    }
+}
