8234773b79f05a7e9c434155f8ed22255235c6dc
Eric.Cui <465785021@qq.com>
2021-05-18 21:49:35 +0800

    sortList

--- src/main/java/org/cxz/algorithm/linkedlist/case148.java
+++ src/main/java/org/cxz/algorithm/linkedlist/case148.java
@@ -8,6 +8,32 @@ package org.cxz.algorithm.linkedlist;
  * @date 2021/5/18 11:17
  */
 public class case148 {
+    public static void main(String[] args) {
 
-    
+    }
+    public ListNode sortList(ListNode head) {
+        if(head==null){
+            return null ;
+        }
+        ListNode ret =new ListNode(0);
+        ret.next = head;
+        int max = head.val;
+        int min = head.val;
+        int mid = 0;
+        ListNode p = head.next;
+        while(p!=null){
+            if(p.val<min){
+                min = p.val;
+            }
+            if(p.val>max){
+                max = p.val;
+            }
+            p =p.next;
+        }
+        mid = (max+min)>>1;
+
+
+
+
+    }
 }
