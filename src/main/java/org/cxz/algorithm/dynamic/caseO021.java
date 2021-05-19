2acb4184748130e06f29f041ee31b5a25d04d61f
Eric.Cui <465785021@qq.com>
2021-05-20 00:31:03 +0800

    exchange

--- src/main/java/org/cxz/algorithm/dynamic/caseO021.java
+++ src/main/java/org/cxz/algorithm/dynamic/caseO021.java
@@ -8,4 +8,40 @@ package org.cxz.algorithm.dynamic;
  * @date 2021/5/20 0:17
  */
 public class caseO021 {
+
+    public static void main(String[] args) {
+
+
+    }
+
+
+
+    public int[] exchange(int[] nums) {
+        process(nums,0,nums.length-1);
+        return nums;
+    }
+
+    public void process(int[] arr, int l,int r){
+        if(l>=r){
+            return;
+        }
+        while (l<r){
+            while(l<r&&arr[l]%2==1){
+                l++;
+            }
+            while(l<r&&arr[r]%2==0){
+                r--;
+            }
+            if (l<r){
+                int p =0;
+                p = arr[l];
+                arr[l] = arr[r];
+                arr[r] = p;
+                l++;
+                r--;
+            }
+        }
+
+    }
+
 }
