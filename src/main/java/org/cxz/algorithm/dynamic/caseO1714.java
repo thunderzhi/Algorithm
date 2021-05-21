8d03b3d6bea4471ca3a1093479a2ac72fdd8f938
Eric.Cui <465785021@qq.com>
2021-05-21 21:39:33 +0800

    1714

---
+++ src/main/java/org/cxz/algorithm/dynamic/caseO1714.java
@@ -0,0 +1,18 @@
+package org.cxz.algorithm.dynamic;
+
+/**
+ * @author cxz
+ * @Title:
+ * @Package
+ * @Description:
+ * @date 2021/5/21 16:51
+ */
+public class caseO1714 {
+    public static void main(String[] args) {
+
+
+    }
+    public int[] smallestK(int[] arr, int k) {
+
+    }
+}
