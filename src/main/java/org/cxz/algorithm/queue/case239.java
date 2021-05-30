cebfd22b63c1a4d8c65d3af0604015ab272acc8f
Eric.Cui <465785021@qq.com>
2021-05-30 18:24:38 +0800

    maxSlidingWindow

--- src/main/java/org/cxz/algorithm/queue/case239.java
+++ src/main/java/org/cxz/algorithm/queue/case239.java
@@ -1,5 +1,8 @@
 package org.cxz.algorithm.queue;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 /**
  * @author cxz
  * @Title:
@@ -8,6 +11,32 @@ package org.cxz.algorithm.queue;
  * @date 2021/5/30 17:53
  */
 public class case239 {
+    public static void main(String[] args) {
 
-    
+    }
+
+
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int[] ans = new int[nums.length-k+1];
+        if (k==0){
+            return new int[]{};
+        }
+        Deque<Integer> q = new LinkedList<>();
+        int ind =0;
+        while (ind<nums.length){
+            if (!q.isEmpty()&&q.peekFirst()+k<=ind){
+                q.pollFirst();
+            }
+            while (!q.isEmpty()&&nums[q.peekLast()]<nums[ind]){
+                q.pollLast();
+            }
+            q.offerLast(ind);
+            ind++;
+            if(ind>=k){
+                ans[ind-k]=nums[q.peekFirst()];
+            }
+        }
+        return ans;
+    }
+
 }
