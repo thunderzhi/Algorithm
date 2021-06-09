e45c89621edce88eb35e7c93a821a6c807fb7535
Eric.Cui <465785021@qq.com>
2021-06-10 01:45:39 +0800

    tt

--- src/main/java/org/cxz/algorithm/sort/case315.java
+++ src/main/java/org/cxz/algorithm/sort/case315.java
@@ -1,6 +1,7 @@
 package org.cxz.algorithm.sort;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -27,31 +28,67 @@ public class case315 {
     public static void main(String[] args) {
         int[] a = new int[]{5,2,6,1};
 
-        new case315().countSmaller(a);
+        List<Integer> res = new case315().countSmaller(a);
+
+        System.out.println("res = " + Arrays.asList(res));
 
     }
 
     public List<Integer> countSmaller(int[] nums) {
-        List<Data> data = new ArrayList<>();
+        List<Data> arr = new ArrayList<>();
         temp = new ArrayList<>();
-        while(temp.size()<nums.length){
+        for (int i = 0; i <nums.length; i++) {
+            arr.add(new Data(i,0,nums[i]));
             temp.add(new Data(0,0,0));
         }
 
 
+
         System.out.println("temp = " + temp);
-        merge(nums,0,nums.length-1);
+        merge(arr,0,nums.length-1);
+        int[] res = new int[nums.length];
+        List<Integer> ret = new ArrayList<>();
+        for (int i = 0; i < arr.size(); i++) {
+            res[arr.get(i).ind] = arr.get(i).cnt;
+        }
+        for (int i = 0; i < res.length; i++) {
+            ret.add(res[i]);
+        }
 
-
-        return new ArrayList<>();
+        return ret;
     }
 
-    public void merge(int[] nums,int l ,int r){
+    public void merge(List<Data> arr,int l ,int r){
         if(l>=r){
             return;
         }
-        
+        int mid =(l+r)>>1;
+        merge(arr,l,mid);
+        merge(arr,mid+1,r);
+        int k = l ,p1 = l, p2 = mid+1;
+        while(p1<=mid||p2<=r){
+            if((p2>r)||(p1<=mid&&arr.get(p1).val>arr.get(p2).val)){
+                Data data = temp.get(p1);
+                data.cnt += r-p2+1;
+                data.val = arr.get(p1).val;
+                k++;
+                p1++;
+            }
+            else {
+                Data data = temp.get(p2);
+                data.val = arr.get(p2).val;
+                k++;
+                p2++;
+            }
+        }
+        for (int i = l; i <=r ; i++) {
+            Data a1 = arr.get(i);
+            Data t1 = temp.get(i);
+            a1.cnt = t1.cnt;
+            a1.ind = t1.ind;
+            a1.val = t1.val;
 
-
+        }
+        return ;
     }
 }
