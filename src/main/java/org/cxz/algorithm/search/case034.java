fbef7d5079437ec9b2e6a84c2ac0a7260e39ad80
Eric.Cui <465785021@qq.com>
2021-06-21 22:48:07 +0800

    searchRange

--- src/main/java/org/cxz/algorithm/search/case034.java
+++ src/main/java/org/cxz/algorithm/search/case034.java
@@ -12,21 +12,44 @@ import java.util.Arrays;
 public class case034 {
     public static void main(String[] args) {
         int[] arr = new int[]{5,7,7,8,8,10};
-        int[] ints = new case034().searchRange(arr, 8);
+        int[] ints = new case034().searchRange(arr, 6);
         System.out.println("Arrays.toString(ints) = " + Arrays.toString(ints));
     }
     public int[] searchRange(int[] nums, int target) {
         int[] ans = new int[]{-1,-1};
         int pos = 0;
-        int first = searchfirst(nums,pos, target);
-        if(first==-1){
+        int first = searchfirst01(nums,pos, target);
+        if(first==nums.length||nums[first]!=target){
             return ans;
         }
         pos = first;
-        int last = searchlast(nums, pos,target);
+        int last = searchfirst01(nums, pos,target+1)-1;
         ans[0] = first;
         ans[1] = last;
         return ans;
+    }
+    public int searchfirst01(int[] arr,int pos, int target){
+        if(arr.length==0){
+            return -1;
+        }
+        int l = pos ;
+        int r = arr.length-1;
+        int mid = 0;
+        while (r-l>3){
+            mid = (l+r)>>1;
+            if(arr[mid]>=target){
+                r = mid;
+            }
+            else{
+                l =mid+1;
+            }
+        }
+        for (int i = l; i <=r ; i++) {
+            if (arr[i]>=target){
+                return i;
+            }
+        }
+        return arr.length;
     }
 
     public int searchlast(int[] arr,int pos, int target){
@@ -43,24 +66,5 @@ public class case034 {
             }
         }
         return arr[l]!= target?-1:l;
-    }
-
-    public int searchfirst(int[] arr,int pos, int target){
-        if(arr.length==0){
-            return -1;
-        }
-        int l = pos ;
-        int r = arr.length-1;
-        int mid = 0;
-        while (l<r){
-            mid = (l+r)>>1;
-            if(arr[mid]>=target){
-                r = mid;
-            }
-            else{
-                l =mid+1;
-            }
-        }
-        return arr[l]!=target?-1:l;
     }
 }
