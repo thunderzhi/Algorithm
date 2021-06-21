cc9c20eb797e02fd50938dedfece762879b71d87
Eric.Cui <465785021@qq.com>
2021-06-21 14:46:30 +0800

    newton method

--- src/main/java/org/cxz/algorithm/search/case069.java
+++ src/main/java/org/cxz/algorithm/search/case069.java
@@ -31,6 +31,21 @@ public class case069 {
 
     }
 
+    public int nt(int x){
+        if (x == 0) {
+            return 0;
+        }
+        double C = x, x0 = x;
+        while (true) {
+            double xi = 0.5 * (x0 + C / x0);
+            if (Math.abs(x0 - xi) < 1e-7) {
+                break;
+            }
+            x0 = xi;
+        }
+        return (int) x0;
+    }
+
 
 
 
