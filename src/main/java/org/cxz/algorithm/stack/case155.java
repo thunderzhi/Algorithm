21a0cbf140d763f747c3c09b06ec33c62ae5a1e7
Eric.Cui <465785032@qq.com>
2021-08-25 17:24:29 +0800

    minstack

---
+++ src/main/java/org/cxz/algorithm/stack/case155.java
@@ -0,0 +1,49 @@
+package org.cxz.algorithm.stack;
+
+import java.util.Stack;
+
+/**
+ * @author cxz
+ * @Title:
+ * @Package
+ * @Description:
+ * @date 2021/8/25 17:23
+ */
+public class case155 {
+
+    public Stack<Integer> stack;
+    public Stack<Integer> minstack;
+    public case155() {
+        stack = new Stack<>();
+        minstack = new Stack<>();//save min
+    }
+
+    public void push(int val) {
+        stack.push(val);
+        if (minstack==null||minstack.peek()>=val)
+        {
+            minstack.push(val);
+        }
+    }
+
+    public void pop() {
+        if (stack.isEmpty()) {
+            return;
+        }
+
+        int pop = (int)stack.pop();
+        if (minstack.peek()==pop){
+            minstack.pop();
+        }
+    }
+
+    public int top() {
+
+        return stack.peek();
+    }
+
+    public int getMin() {
+
+        return  minstack.peek();
+    }
+}
