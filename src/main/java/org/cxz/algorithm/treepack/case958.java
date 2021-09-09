522203c965c3f064c094dd796dccac4aa9e91db8
Eric.Cui <465785032@qq.com>
2021-09-09 11:35:30 +0800

    old

--- src/main/java/org/cxz/algorithm/treepack/case958.java
+++ src/main/java/org/cxz/algorithm/treepack/case958.java
@@ -1,5 +1,8 @@
 package org.cxz.algorithm.treepack;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * @author cxz
  * @Title:
@@ -33,4 +36,49 @@ public class case958 {
         return left+right+1;
 
     }
+
+    ///BFS  BFS when u get first null,after that u get another not null node
+    // it represents that its not a completeTree
+    public boolean isCompleteTreeBFS(TreeNode root){
+        if(root==null){
+            return true;
+        }
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        boolean reachnull = false;
+        queue.offer(root);
+        while(!queue.isEmpty()){
+
+            TreeNode tmp = queue.poll();
+            if (tmp==null){
+                reachnull = true;
+            }
+            else {
+                if(reachnull){
+                    return false;
+                }
+
+                System.out.println("Poll "+tmp.val);
+                TreeNode left = tmp.left;
+                TreeNode right = tmp.right;
+                queue.offer(left);
+                queue.offer(right);
+            }
+
+        }
+        return true;
+    }
+
+    public boolean isCompleteTreeRecursion(TreeNode root){
+        //1 get the total node count of the tree
+        // 2 use formula to validate
+        // 3 recursive
+
+
+
+
+
+    }
+
+
+
 }
