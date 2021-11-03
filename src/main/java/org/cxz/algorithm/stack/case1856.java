2178bf587e968a384b61a671fa3f50480f935443
Eric.Cui <465785032@qq.com>
2021-11-03 18:07:13 +0800

    1856

---
+++ src/main/java/org/cxz/algorithm/stack/case1856.java
@@ -0,0 +1,18 @@
+package org.cxz.algorithm.stack;
+
+/**
+ * @author cxz
+ * @Title:
+ * @Package
+ * @Description:
+ * @date 2021/11/3 17:57
+ */
+public class case1856 {
+    public static void main(String[] args) {
+
+    }
+
+    public int maxSumMinProduct(int[] nums) {
+        return 1;
+    }
+}
