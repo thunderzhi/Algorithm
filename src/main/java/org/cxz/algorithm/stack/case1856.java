530ba59f8bce13fa8623c829d9c76095503069aa
Eric.Cui <465785032@qq.com>
2021-11-04 11:36:00 +0800

    increase

--- src/main/java/org/cxz/algorithm/stack/case1856.java
+++ src/main/java/org/cxz/algorithm/stack/case1856.java
@@ -1,5 +1,9 @@
 package org.cxz.algorithm.stack;
 
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.LinkedList;
+
 /**
  * @author cxz
  * @Title:
@@ -12,7 +16,38 @@ public class case1856 {
 
     }
 
+    //monotonic increase stack
     public int maxSumMinProduct(int[] nums) {
-        return 1;
+        long[] sum = new long[nums.length+1];
+//        long mod = 1000000007;
+        long mod =(long)(1e9+7);
+        sum[0]=0;
+        for(int i=0;i<nums.length;i++){
+            sum[i+1]= sum[i]+nums[i];
+        }
+        int[] l = new int[nums.length];
+        Arrays.fill(l,-1);
+        int[] r = new int[nums.length];
+        Arrays.fill(r,nums.length);
+        Deque<Integer> st = new LinkedList<>();
+        for (int i = 0; i < nums.length; i++) {
+            while(!st.isEmpty()&& nums[i]<=nums[st.peekLast()]){
+                r[st.peekLast()] =i;
+                st.pollLast();
+            }
+            if(!st.isEmpty()){
+                l[i] = st.peekLast();
+            }
+            st.offerLast(i);
+        }
+        long ans = 0;
+        for (int i = 0; i < nums.length; i++) {
+
+            long a = nums[i] * (sum[r[i]] - sum[l[i]+1]);
+            ans = Math.max(ans,a);
+        }
+
+        return (int)(ans%mod);
+
     }
 }
---
+++ src/main/java/org/cxz/algorithm/stack/case907.java
@@ -0,0 +1,18 @@
+package org.cxz.algorithm.stack;
+
+/**
+ * @author cxz
+ * @Title:
+ * @Package
+ * @Description:
+ * @date 2021/11/4 11:35
+ */
+public class case907 {
+    public static void main(String[] args) {
+
+    }
+
+    public int sumSubarrayMins(int[] arr) {
+
+    }
+}
