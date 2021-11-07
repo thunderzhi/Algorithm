1c8e813607de018d02f95a97505639d2204955c9
Eric.Cui <465785032@qq.com>
2021-11-07 23:00:33 +0800

    395

---
+++ src/main/java/org/cxz/algorithm/string/case395.java
@@ -0,0 +1,18 @@
+package org.cxz.algorithm.string;
+
+/**
+ * @author cxz
+ * @Title:
+ * @Package
+ * @Description:
+ * @date 2021/11/7 22:59
+ */
+public class case395 {
+    public static void main(String[] args) {
+
+    }
+
+    public int longestSubstring(String s, int k) {
+        return  1;
+    }
+}
