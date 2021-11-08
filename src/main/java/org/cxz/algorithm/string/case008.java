483357746eb0f4d2e1c70b1f347b1abbe1259f2d
Eric.Cui <465785032@qq.com>
2021-11-08 09:12:25 +0800

    myAtoi

--- src/main/java/org/cxz/algorithm/string/case008.java
+++ src/main/java/org/cxz/algorithm/string/case008.java
@@ -13,6 +13,39 @@ public class case008 {
     }
 
     public int myAtoi(String s) {
-
+        if(s==null||s.length()==0){
+            return 0;
+        }
+        int flag= 1;
+        int premax = Integer.MAX_VALUE/10;
+        int d = Integer.MAX_VALUE%10;
+        int ind =0;
+        while(ind<s.length()&&s.charAt(ind)==' '){//miss ind out of bound
+            ind++;
+        }
+        if(ind==s.length()){
+            return 0;//miss check
+        }
+        if(s.charAt(ind)=='-'){
+            flag =-1;
+            ind++;//miss
+        }
+        else if(s.charAt(ind)=='+'){//miss
+            ind++;
+        }
+        int num =0;
+        for(int i = ind;i<s.length();i++){
+            if(s.charAt(i)<'0'||s.charAt(i)>'9'){ //miss
+                break;//miss
+            }
+            if(num>premax||(num==premax&&(s.charAt(i)-'0')>d)){//wrong
+                if(flag>0){
+                    return Integer.MAX_VALUE;
+                }
+                return Integer.MIN_VALUE;
+            }
+            num = num*10+(s.charAt(i)-'0');
+        }
+        return num*flag;
     }
 }
