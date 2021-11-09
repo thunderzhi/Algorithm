1e68ffbe4e5019160e3c8d6910467fb6fd424f9f
Eric.Cui <465785032@qq.com>
2021-11-10 00:05:29 +0800

    1081

--- src/main/java/org/cxz/algorithm/stack/case1081.java
+++ src/main/java/org/cxz/algorithm/stack/case1081.java
@@ -1,5 +1,8 @@
 package org.cxz.algorithm.stack;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 /**
  * @author cxz
  * @Title:
@@ -13,6 +16,29 @@ public class case1081 {
     }
 
     public String smallestSubsequence(String s) {
-
+        int[] cnt = new int[26];
+        boolean[] h = new boolean[26];
+        Deque<Character> qu = new ArrayDeque<>();
+        for(int i= 0;i<s.length();i++){
+            char c = s.charAt(i);
+            cnt[s.charAt(i)-'a']+=1;
+        }
+        for(int i= 0;i<s.length();i++){
+            char cur = s.charAt(i);
+            if(!h[cur-'a']){//miss
+                while(!qu.isEmpty()&&qu.peekLast()>cur&&cnt[qu.peekLast()-'a']>0){
+                    char c = qu.pollLast();
+                    h[c-'a']= false;
+                }
+                qu.offerLast(cur);
+                h[cur-'a']=true;
+            }
+            cnt[cur-'a']--;
+        }
+        StringBuilder sb = new StringBuilder();
+        while(!qu.isEmpty()){
+            sb.append(qu.pollFirst());
+        }
+        return sb.toString();
     }
 }
