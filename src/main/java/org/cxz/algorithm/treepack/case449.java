90573812c2bfc907cbd3b16aa1205028635e1e96
Eric.Cui <465785032@qq.com>
2021-11-19 17:59:20 +0800

    449

---
+++ src/main/java/org/cxz/algorithm/treepack/case449.java
@@ -0,0 +1,31 @@
+package org.cxz.algorithm.treepack;
+
+/**
+ * @author cxz
+ * @Title:
+ * @Package
+ * @Description:
+ * @date 2021/11/19 16:21
+ */
+public class case449 {
+    public class Codec {
+
+        // Encodes a tree to a single string.
+        public String serialize(TreeNode root) {
+            //preorder
+        }
+
+        // Decodes your encoded data to tree.
+        public TreeNode deserialize(String data) {
+
+        }
+    }
+
+
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
