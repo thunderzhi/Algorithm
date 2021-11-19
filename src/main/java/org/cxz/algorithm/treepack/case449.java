aa831f75f224ab027d21333f9edade333823f3c1
Eric.Cui <465785032@qq.com>
2021-11-20 00:12:33 +0800

    serialize

--- src/main/java/org/cxz/algorithm/treepack/case449.java
+++ src/main/java/org/cxz/algorithm/treepack/case449.java
@@ -12,6 +12,29 @@ public class case449 {
 
         // Encodes a tree to a single string.
         public String serialize(TreeNode root) {
+            if(root==null){
+                return "";
+            }
+            StringBuilder sb = new StringBuilder();
+            sb.append(root.val);
+            if(root.left!=null||root.right!=null){
+                sb.append("(");
+            }
+            sb.append(serialize(root.left));
+//            String left ="";
+//            left = serialize(root.left);
+            if(root.right!=null){
+                sb.append(",");
+            }
+            sb.append(serialize(root.right));
+//            String rigth ="";
+//            rigth = serialize(root.right);
+            if(root.left!=null||root.right!=null){
+                sb.append(")");
+            }
+            System.out.println("sb = " + sb.toString());
+            return sb.toString();
+
             //preorder
         }
 
