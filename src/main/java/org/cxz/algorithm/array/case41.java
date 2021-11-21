6b6344fb1cb33bf4d8d0101e01784329152514ba
Eric.Cui <465785032@qq.com>
2021-11-21 16:16:38 +0800

    firstMissingPositive

--- src/main/java/org/cxz/algorithm/array/case41.java
+++ src/main/java/org/cxz/algorithm/array/case41.java
@@ -11,7 +11,34 @@ public class case41 {
     public static void main(String[] args) {
 
     }
-    public int firstMissingPositive(int[] nums) {
-
+    public void swap(int[] arr,int i,int j){
+        int t = arr[i];
+        arr[i] = arr[j];
+        arr[j] = t;
     }
+
+    public int firstMissingPositive(int[] nums) {
+        int n = nums.length;
+        for (int i = 0;i<nums.length;i++){
+            while(nums[i]!=i+1){
+                if(nums[i]>n||nums[i]<=0){
+                    break;
+                }
+                int ind = nums[i]-1;
+                if(nums[i]==nums[ind]){
+                    break;
+                }
+                swap(nums,i,ind);
+            }
+        }
+        int ind =0;
+        //cannot use for, cause the miss nums could bigger than length
+        while(ind<nums.length&&nums[ind]==ind+1){
+            ind++;
+        }
+        return ind+1;
+    }
+
+
+
 }
