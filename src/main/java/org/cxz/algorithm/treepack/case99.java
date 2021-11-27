970820c9f7877147b33a09d1bf5fa7fab9a53547
Eric.Cui <465785032@qq.com>
2021-11-27 21:39:04 +0800

    recoverTree

--- src/main/java/org/cxz/algorithm/treepack/case99.java
+++ src/main/java/org/cxz/algorithm/treepack/case99.java
@@ -11,8 +11,36 @@ public class case99 {
     public static void main(String[] args) {
 
     }
-
+    TreeNode pre;
+    TreeNode p;
+    TreeNode q;
     public void recoverTree(TreeNode root) {
+        pre = q = p =null;
+        inorder(root);
+        if(p!=null&&q!=null){
+            swap(p,q);
+            return;
+        }
+    }
+    public void inorder(TreeNode root){
+        if (root==null){
+            return;
+        }
+        inorder(root.left);
+        if (pre!=null&&pre.val>root.val){
+            if(p==null){
+                p = pre;
+            }
+            q = root;
+        }
+        pre =root;
+        inorder(root.right);
+        return;
+    }
 
+    public void swap(TreeNode a,TreeNode b){
+        int tmp =a.val ;
+        a.val= b.val;
+        b.val =tmp;
     }
 }
