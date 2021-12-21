35ccc5c8226366b8e9ff400ea4929279f931fdd6
Eric.Cui <465785032@qq.com>
2021-12-21 23:01:40 +0800

    ddd

--- src/main/java/org/cxz/algorithm/array/case611.java
+++ src/main/java/org/cxz/algorithm/array/case611.java
@@ -63,4 +63,23 @@ public class case611 {
         return arr.length;
     }
 
+    //another method double point
+    public int doublePoint(int[] nums){
+        int n = nums.length;
+        int ans =0;
+        for (int i =n-1; i >1 ; i--) {
+            int l = 0;
+            int r =i-1;
+            while (l<r){
+                if(nums[l]+nums[r]>nums[i]){
+                    ans +=r-l;
+                    r-=1;
+                }
+                else{
+                    l+=1;
+                }
+            }
+        }
+        return ans;
+    }
 }
