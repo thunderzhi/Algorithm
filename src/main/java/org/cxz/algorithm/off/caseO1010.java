11d7aae28e83cdf6d02f239fb23975902c61e76d
Eric.Cui <465785032@qq.com>
2022-01-02 15:09:53 +0800

    1010

--- src/main/java/org/cxz/algorithm/off/caseO1010.java
+++ src/main/java/org/cxz/algorithm/off/caseO1010.java
@@ -8,4 +8,47 @@ package org.cxz.algorithm.off;
  * @date 2022/1/2 11:46
  */
 public class caseO1010 {
+
+    class StreamRank {
+        public int[] C;
+        public int n;
+
+        public StreamRank() {
+            n = 50001;
+            C = new int[n+1];
+        }
+
+        public int lowbit(int x){
+            return x&(-x);
+        }
+
+        public void add(int i,int x){
+            while (i<=n){
+                C[i]+=x;
+                i+= lowbit(i);
+            }
+            return ;
+        }
+
+        public int query(int i){
+            int sum =0;
+            while(i>0){
+                sum += C[i];
+                i-=lowbit(i);
+            }
+            return sum;
+        }
+
+        public void track(int x) {
+            add(x+1,1);
+        }
+
+        public int getRankOfNumber(int x) {
+            return query(x+1);
+        }
+    }
+
+
+
+
 }
