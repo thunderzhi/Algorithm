6de7f6a03ff51204968b90a29f0ad3e59378d075
Eric.Cui <465785032@qq.com>
2022-01-11 14:51:55 +0800

    494

--- src/main/java/org/cxz/algorithm/recurrence/case494.java
+++ src/main/java/org/cxz/algorithm/recurrence/case494.java
@@ -12,5 +12,16 @@ public class case494 {
 
     }
 
-    
+    public int findTargetSumWays(int[] nums, int target) {
+        /*
+        f[i][j] = f[i − 1][j − x] + f[i − 1][j + x]
+        f[ind][j + x] += f[pre_ind][j];
+        f[ind][j - x] += f[pre_ind][j];
+        f[i][j + x] =f[i][j + x] + f[i-1][j];
+        f[i][j - x] =f[i][j - x] + f[i-1][j];
+        * */
+       // int[][] dp =
+
+        return 1;
+    }
 }
