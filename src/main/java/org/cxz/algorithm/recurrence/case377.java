6a7601547b25ec22a8f96fbfbc5c3cab50920b42
Eric.Cui <465785032@qq.com>
2022-01-12 00:00:07 +0800

    new

---
+++ src/main/java/org/cxz/algorithm/recurrence/case377.java
@@ -0,0 +1,17 @@
+package org.cxz.algorithm.recurrence;
+
+/**
+ * @author cxz
+ * @Title:
+ * @Package
+ * @Description:
+ * @date 2022/1/11 23:58
+ */
+public class case377 {
+    public static void main(String[] args) {
+
+    }
+    public int combinationSum4(int[] nums, int target) {
+        return 1 ;
+    }
+}
---
+++ src/main/java/org/cxz/algorithm/recurrence/case518.java
@@ -0,0 +1,18 @@
+package org.cxz.algorithm.recurrence;
+
+/**
+ * @author cxz
+ * @Title:
+ * @Package
+ * @Description:
+ * @date 2022/1/11 23:58
+ */
+public class case518 {
+    public static void main(String[] args) {
+        
+    }
+    public int change(int amount, int[] coins) {
+        return 1 ;
+
+    }
+}
