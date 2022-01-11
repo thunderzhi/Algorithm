3faa457245dd172cce01f40860c7b20bdc0a1516
Eric.Cui <465785032@qq.com>
2022-01-11 12:22:19 +0800

    recurrence case494

---
+++ src/main/java/org/cxz/algorithm/recurrence/case494.java
@@ -0,0 +1,16 @@
+package org.cxz.algorithm.recurrence;
+
+/**
+ * @author cxz
+ * @Title:
+ * @Package
+ * @Description:
+ * @date 2022/1/11 11:55
+ */
+public class case494 {
+    public static void main(String[] args) {
+
+    }
+
+    
+}
