38a76660da4aa2f7eedabfe26ba6309fe9a61fd1
Eric.Cui <465785032@qq.com>
2022-01-12 14:29:40 +0800

    518

--- src/main/java/org/cxz/algorithm/recurrence/case377.java
+++ src/main/java/org/cxz/algorithm/recurrence/case377.java
@@ -12,6 +12,19 @@ public class case377 {
 
     }
     public int combinationSum4(int[] nums, int target) {
-        return 1 ;
+        /*
+        f[j] represent use all num from nums to get sum ==j
+        f[j] = f[j-x=0]+f[j-x=1]+f[j-x=2]+f[j-x=3]+f[j-x=4]+ …….+f[j-x=n]//x from nums 0~n
+        */
+        int[] dp = new int[target+1];
+        dp[0]=1;
+        for (int i = 1; i <=target ; i++) {
+            for (int num : nums) {
+                if(i-num<0){continue;}
+                dp[i] += dp[i-num];
+            }
+        }
+
+        return dp[target];
     }
 }
--- src/main/java/org/cxz/algorithm/recurrence/case518.java
+++ src/main/java/org/cxz/algorithm/recurrence/case518.java
@@ -9,10 +9,17 @@ package org.cxz.algorithm.recurrence;
  */
 public class case518 {
     public static void main(String[] args) {
-        
+
     }
     public int change(int amount, int[] coins) {
-        return 1 ;
-
+        int[] dp = new int[amount+1];
+        dp[0]=1;
+        int n = coins.length;
+        for (int x :coins) {
+            for (int j = x; j <=amount ; j++) {
+                dp[j] += dp[j-x];
+            }
+        }
+        return dp[amount];
     }
 }
