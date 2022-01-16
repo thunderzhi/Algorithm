820a4464a8e11ee0b108ea75c862e19f7707ec30
Eric.Cui <465785032@qq.com>
2022-01-16 10:34:54 +0800

    Eratosthenes

--- src/main/java/org/cxz/algorithm/math/prime01.java
+++ src/main/java/org/cxz/algorithm/math/prime01.java
@@ -11,7 +11,11 @@ import com.sun.scenario.effect.impl.prism.PrMergePeer;
  */
 public class prime01 {
 
-
+    public static void main(String[] args) {
+        int n = 100;
+        int res = new prime01().Eratosthenes(n);
+        System.out.println("res = " + res);
+    }
     public final int max =10000;
 
     public int [] mu = new int[max+5];
@@ -29,7 +33,7 @@ public class prime01 {
                     break;
                 }
                 prime[i* prime[j]]=1;
-                if(i*prime[j]==0){
+                if(i%prime[j]==0){
                     break;
                 }
                 mu[i*prime[j]]=-mu[i];
@@ -37,5 +41,26 @@ public class prime01 {
         }
         return;
     }
+
+
+    public int Eratosthenes(int n){
+        int[] prime = new int[n + 1];
+        for (int i = 2; i*i <= n; i++) {
+            if (prime[i] == 1) {
+                continue;
+            }
+            for (int j = i *i; j <= n; j+=i) {
+                prime[j] = 1;
+            }
+        }
+        for (int i = 2; i <=n; i++) {
+            if (prime[i]==0) {
+                prime[0]++;
+            }
+        }
+        return prime[0];
+    }
+
+
 
 }
