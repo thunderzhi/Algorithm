6b3cc379129c9823792fb6bce7f226a593920a06
Eric.Cui <465785032@qq.com>
2022-01-27 17:34:50 +0800

    502

--- src/main/java/org/cxz/algorithm/array/case502.java
+++ src/main/java/org/cxz/algorithm/array/case502.java
@@ -44,4 +44,25 @@ public class case502 {
         }
         return w;
     }
+
+
+    public int findMaximizedCapital2(int k, int w, int[] profits, int[] capital) {
+        Integer[] ind = new Integer[capital.length];
+        for(int i=0;i<ind.length;i++){
+            ind[i]=i;
+        }
+        //sort by capital
+        Arrays.sort(ind, (o1,o2)->{return capital[o1]-capital[o2];});
+        PriorityQueue<Integer> pq = new PriorityQueue<Integer>((o1,o2)->{return o2-o1;});
+        int start =0;
+        for(;k>0;k--){
+            while(start<ind.length&&w>=capital[ind[start]]){
+                pq.offer(profits[ind[start]]);//miss profit
+                start++;
+            }
+            if(pq.size()==0){break;}
+            w+= pq.poll();
+        }
+        return w;
+    }
 }
