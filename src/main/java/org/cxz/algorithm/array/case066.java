a2ecfe1f0fc48d0b622a11e7d6bb93c33d383f1b
Eric.Cui <465785032@qq.com>
2022-02-15 17:32:35 +0800

    66

---
+++ src/main/java/org/cxz/algorithm/array/case066.java
@@ -0,0 +1,45 @@
+package org.cxz.algorithm.array;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * @author cxz
+ * @Title:
+ * @Package
+ * @Description:
+ * @date 2022/2/15 17:30
+ */
+public class case066 {
+
+    //prue moni
+    public int[] plusOne(int[] digits) {
+        int[] ans = new int[digits.length];
+        int p = digits.length-1;
+        Deque<Integer> st = new LinkedList();
+        int mark = 0;
+        while(p>=0){
+            int i = digits[p];
+            if(st.isEmpty()){ i +=1;}
+            if(mark==1){
+                i++;
+                mark =0;
+            }
+            st.offerLast(i%10);
+            mark = i/10;
+            p--;
+        }
+        if(mark==1){
+            ans = new int[digits.length+1];
+            ans[0] =1;
+            for(int i=1;i<ans.length;i++){
+                ans[i] = st.pollLast();
+            }
+            return ans;
+        }
+        for(int i=0;i<ans.length;i++){
+            ans[i] = st.pollLast();
+        }
+        return ans;
+    }
+}
