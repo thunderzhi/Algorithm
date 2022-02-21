1ccbe8e34f1da28de481990cdadc8cf466313ca3
Eric.Cui <465785032@qq.com>
2022-02-21 18:07:52 +0800

    876

--- src/main/java/org/cxz/algorithm/linkedlist/case876.java
+++ src/main/java/org/cxz/algorithm/linkedlist/case876.java
@@ -29,4 +29,14 @@ public class case876 {
         }
         return ret;
     }
+
+    //ver 2
+    public ListNode middleNode2(ListNode head) {
+        ListNode slow = head, fast = head;
+        while(fast.next!=null&&fast.next.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return fast.next==null? slow: slow.next;
+    }
 }
