2ef70894dbdfa858f6f10fac217191437a8911c9
Eric.Cui <465785032@qq.com>
2022-03-03 12:40:18 +0800

    dfs

--- src/main/java/org/cxz/algorithm/treepack/case572.java
+++ src/main/java/org/cxz/algorithm/treepack/case572.java
@@ -9,5 +9,21 @@ package org.cxz.algorithm.treepack;
  */
 public class case572 {
 
-    
+    //ver dfs
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if(root==null&&subRoot==null) return true;
+        if(root==null||subRoot==null) return false;
+        if(root.val==subRoot.val&&match(root,subRoot)){
+            return true;
+        }
+        return isSubtree(root.right,subRoot)
+                ||isSubtree(root.left,subRoot);
+    }
+
+    public boolean match(TreeNode root, TreeNode subRoot){
+        if(root==null&&subRoot==null) return true;
+        if(root==null||subRoot==null) return false;
+        if(root.val!=subRoot.val) return false;
+        return  match(root.right,subRoot.right)&& match(root.left,subRoot.left);
+    }
 }
