49b9fa37f6e0259fcd8a5a33f84052ea6018bcc6
Eric.Cui <465785032@qq.com>
2022-03-10 12:24:31 +0800

    1291

---
+++ src/main/java/org/cxz/algorithm/dynamic/case1291.java
@@ -0,0 +1,57 @@
+package org.cxz.algorithm.dynamic;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author cxz
+ * @Title:
+ * @Package
+ * @Description:
+ * @date 2022/3/10 12:23
+ */
+public class case1291 {
+
+    // ver1 imitation math add
+    public List<Integer> sequentialDigits1(int low, int high) {
+        int num1 = low;
+        int cntlow=0;
+        while(num1>0){
+            num1 /=10;
+            cntlow++;
+        }
+        int num2 = high;
+        int cnthigh=0;
+        while(num2>0){
+            num2 /=10;
+            cnthigh++;
+        }
+        List<Integer> ans = new ArrayList();
+        build(cntlow,cnthigh,low,high,ans);
+        return ans;
+    }
+
+    public void build(int cntlow , int cnthigh, int low, int high, List<Integer> ans){
+        int[] step = new int[10];
+        step[1]=1;
+        int[] orign = new int[10];
+        orign[1]=1;
+        for(int i=2;i<=9;i++){
+            orign[i] = orign[i-1]*10+i;//1,12,123,1234
+            step[i] = step[i-1]*10+1;//1,11,111,1111
+        }
+        for(int i= cntlow;i<=cnthigh;i++){
+            if(i==10) break;
+            int max = Math.min(high,step[i]*9);
+            int num = orign[i];
+            while(num<=max){
+                // if(num%10==9) break;
+                if(num>=low&&num<=high){
+                    ans.add(num);
+                }
+                if(num%10==9) break;
+                num+=step[i];
+            }
+        }
+    }
+}
