7186acbc2b64d9a2adf1b39a99712a22b48d04b7
Eric.Cui <465785032@qq.com>
2022-03-16 11:45:57 +0800

    findRepeatNumber

---
+++ src/main/java/org/cxz/algorithm/off/caseOff003.java
@@ -0,0 +1,38 @@
+package org.cxz.algorithm.off;
+
+/**
+ * @author cxz
+ * @Title:
+ * @Package
+ * @Description:
+ * @date 2022/3/16 11:44
+ */
+public class caseOff003 {
+
+    // ver 1 O1
+    public int findRepeatNumber(int[] nums) {
+        int n = nums.length;
+        for(int i =0;i<n;i++){
+            if(i==nums[i]) continue;
+            int res = recovery(i,nums);
+            if(res!=-1) return res;
+        }
+        return -1;
+    }
+
+    public int recovery(int x,int[] nums){
+        while(x != nums[x]){
+            int ind = nums[x];
+            if(ind == nums[ind]) return ind;
+            swap(nums,ind,x);
+        }
+        return -1;
+    }
+
+    public void swap(int[] arr,int i ,int j){
+        int t = arr[i];
+        arr[i] = arr[j];
+        arr[j] = t;
+        return;
+    }
+}
