53bfcc11a84fe20086312b9cfa003122d2cd320b
Eric.Cui <465785032@qq.com>
2022-03-20 00:16:03 +0800

    test

---
+++ src/main/java/org/cxz/algorithm/dynamic/test1111111.java
@@ -0,0 +1,19 @@
+package org.cxz.algorithm.dynamic;
+
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+
+/**
+ * @author cxz
+ * @Title:
+ * @Package
+ * @Description:
+ * @date 2022/3/19 23:08
+ */
+public class test1111111 {
+
+    public static void main(String[] args) {
+        new ArrayList();
+        PriorityQueue<Double> pq = new PriorityQueue<Double>((o1, o2)->{return o2.compareTo(o1);});
+    }
+}
