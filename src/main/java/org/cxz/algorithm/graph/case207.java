81c2d5bc0019f251fed21ce253a505579f017b9d
Eric.Cui <465785032@qq.com>
2022-04-23 12:43:08 +0800

    tupusort

--- src/main/java/org/cxz/algorithm/graph/case207.java
+++ src/main/java/org/cxz/algorithm/graph/case207.java
@@ -1,5 +1,7 @@
 package org.cxz.algorithm.graph;
 
+import java.util.*;
+
 /**
  * @author cxz
  * @Title:
@@ -7,6 +9,34 @@ package org.cxz.algorithm.graph;
  * @Description:
  * @date 2022/4/23 0:33
  */
-public class case270 {
-    
+public class case207 {
+
+    //ver1 topo BFS
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        int[] indegree = new int[numCourses];
+        Queue<Integer> queue = new LinkedList();
+        HashMap<Integer,List<Integer>> g = new HashMap();
+        for(int[] x :prerequisites){
+            int from = x[1];
+            int to = x[0];
+            if(!g.containsKey(from)) g.put(from,new ArrayList());
+            g.get(from).add(to);
+            indegree[to]++;
+        }
+        for(int i =0;i<numCourses; i++){
+            if(indegree[i]==0) queue.offer(i);
+        }
+        int ans = 0;
+        while(!queue.isEmpty()){
+            int t = queue.poll();
+            ans++;
+            List<Integer> l = g.get(t);
+            if(l==null||l.size()==0) continue;
+            for(int i: l){
+                indegree[i]--;
+                if(indegree[i]==0) queue.offer(i);
+            }
+        }
+        return ans==numCourses;
+    }
 }
