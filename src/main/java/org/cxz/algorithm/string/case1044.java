655907764331572a4ed07345bf7a741553a18723
Eric.Cui <465785032@qq.com>
2022-08-17 16:09:23 +0800

    hash string

---
+++ src/main/java/org/cxz/algorithm/string/case1044.java
@@ -0,0 +1,44 @@
+package org.cxz.algorithm.string;
+
+/**
+ * @author cxz
+ * @Title:
+ * @Package
+ * @Description: hash string 
+ * @date 2022/8/17 16:05
+ */
+public class case1044 {
+	long[] h, p;
+    public String longestDupSubstring(String s) {
+        int P = 1313131, n = s.length();
+        h = new long[n + 10]; p = new long[n + 10];
+        p[0] = 1;
+        for (int i = 0; i < n; i++) {
+            p[i + 1] = p[i] * P;
+            h[i + 1] = h[i] * P + s.charAt(i);
+        }
+        String ans = "";
+        int l = 0, r = n;
+        while (l < r) {
+            int mid = l + r + 1 >> 1;
+            String t = check(s, mid);
+            if (t.length() != 0) l = mid;
+            else r = mid - 1;
+            ans = t.length() > ans.length() ? t : ans;
+        }
+        return ans;
+    }
+    String check(String s, int len) {
+        int n = s.length();
+        Set<Long> set = new HashSet<>();
+        for (int i = 1; i + len - 1 <= n; i++) {
+            int j = i + len - 1;
+            long cur = h[j] - h[i - 1] * p[j - i + 1];
+            if (set.contains(cur)) return s.substring(i - 1, j);
+            set.add(cur);
+        }
+        return "";
+    }
+}
+
+ 
