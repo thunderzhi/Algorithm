6d836fc3eefc142b22900c42d32a15ca5f6e2641
Eric.Cui <465785032@qq.com>
2022-11-11 15:39:46 +0800

    ticket

--- src/main/java/org/cxz/thread/ticket.java
+++ src/main/java/org/cxz/thread/ticket.java
@@ -1,11 +1,20 @@
 package org.cxz.thread;
 
+import javax.lang.model.SourceVersion;
+
 /**
- * @author cxz
+ * @author  
  * @Title:
  * @Package
  * @Description:
  * @date 2022/10/27 11:06
  */
 public class ticket {
+
+    public int cnt=300000;
+    public void sale(){
+        cnt--;
+
+        //System.out.println("T name = " + Thread.currentThread().getName() + " cnt "+ cnt);
+    }
 }
