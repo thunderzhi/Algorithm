d7232c8541ef9a0e60f8a0be7d01dd01e76a2746
Eric.Cui <465785032@qq.com>
2022-12-17 12:05:04 +0800

    一维优化

--- src/main/java/org/cxz/algorithm/array/case1774b.java
+++ src/main/java/org/cxz/algorithm/array/case1774b.java
@@ -1,6 +1,5 @@
 package org.cxz.algorithm.array;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 
 /**
@@ -10,39 +9,36 @@ import java.util.Arrays;
  * @Description:
  * @date 2022/12/4 15:30
  */
-public class case1774 {
+public class case1774b {
 
-    // 朴素的动态规划
+    // 一维优化
     public int closestCost(int[] baseCosts, int[] toppingCosts, int target) {
         int n = toppingCosts.length;
         int[] val = toppingCosts;
         int min = Arrays.stream(baseCosts).min().getAsInt();
         if(min>=target) return min;
         int Cap = 2*target-min;
-        boolean[][] f = new boolean[n+1][Cap+1];
+        boolean[] f = new boolean[Cap+1];
         //f[0][0]=true;
         //System.out.println("Cap = " + Cap);
         for(int i =0;i<baseCosts.length;i++){
             if(baseCosts[i]>Cap) continue;
-            f[0][baseCosts[i]] = true;
+            f[baseCosts[i]] = true;
         }
 
         int ans = min;
-
         for(int i =1;i<=n;i++){
-            for(int j =min;j<=Cap;j++){
-                //剪枝 如果j>target&& j-target>target - ans 就不用继续了
-                //if(j>target&&(j-target>target - ans)) break;
-                for(int k =0;k<=2;k++){
+            for(int j =Cap;j>0;j--){
+                for(int k =0;k<=2&&j>=k*val[i-1];k++){
                     int t = j-k*val[i-1];
-                    if(t>=0){
-                        //System.out.println("f["+(i-1)+"]["+t+"] = " + f[i-1][t]);
-                        f[i][j] = f[i][j] |f[i-1][t];
-                    }
+//                    System.out.println("k= "+k+" val["+(i-1)+"]="+val[i-1]+" i= "+i+" f[t:"+t+"] = " + f[t]);
+                    f[j] |= f[t];
+//                    System.out.println("--> k= "+k+" val["+(i-1)+"]="+val[i-1]+" i= "+i+" f[j:"+j+"] = " + f[j]);
                 }
-                if(f[i][j]){
-                    // System.out.println("-------");
-                    //  System.out.println("TTT f["+i+"]["+j+"] = " + f[i][j]);
+                //System.out.println("-------" + f[j]);
+                if(f[j]){
+//                    System.out.println("-------");
+//                    System.out.println("TTT f["+j+"] = " + f[j]);
                     if(j==target) return target;
                     if(Math.abs(j-target)<=Math.abs(ans-target)){
                         if(Math.abs(j-target)==Math.abs(ans-target)){
@@ -51,11 +47,9 @@ public class case1774 {
                         else{
                             ans = j;
                         }
-                        if(j>target) break;
                     }
                 }
             }
-
         }
         return ans;
     }
