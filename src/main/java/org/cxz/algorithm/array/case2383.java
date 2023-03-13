1dcbfa76444d893bd36c212c968f1314e36e4669
Eric.Cui <465785032@qq.com>
2023-03-13 17:35:14 +0800

    minNumberOfHours

---
+++ src/main/java/org/cxz/algorithm/array/case2383.java
@@ -0,0 +1,27 @@
+package org.cxz.algorithm.array;
+
+/**
+ * @author cxz
+ * @Title:
+ * @Package
+ * @Description:
+ * @date 2023/3/13 17:32
+ */
+public class case2383 { 
+    public int minNumberOfHours(int initialEnergy, int initialExperience, int[] energy, int[] experience) {
+        int sum = 0;
+        for (int e : energy) {
+            sum += e;
+        }
+        int trainingHours = initialEnergy > sum ? 0 : sum + 1 - initialEnergy;
+        for (int e : experience) {
+            if (initialExperience <= e) {
+                trainingHours += 1 + (e - initialExperience);
+                initialExperience = 2 * e + 1;
+            } else {
+                initialExperience += e;
+            }
+        }
+        return trainingHours;
+    }
+}
