b850608b8e8fb3ac9b33de5c3369c377860b1eac
Eric.Cui <465785032@qq.com>
2023-07-04 18:40:32 +0800

    makeTheIntegerZero

--- src/main/java/org/cxz/algorithm/bit/case2479.java
+++ src/main/java/org/cxz/algorithm/bit/case2479.java
@@ -1,29 +1,14 @@
-package org.cxz.contest.test351week;
+package org.cxz.algorithm.bit;
 
 /**
  * @author cxz
  * @Title:
  * @Package
  * @Description:
- * @date 2023/6/25 10:28
+ * @date 2023/7/4 18:37
  */
-public class test0625b {
-
+public class case2479 {
     public int makeTheIntegerZero(int num1, int num2) {
-        //num1 = num1 - (2^i+num2)
-        //num1 = num1 - (2^i+num2)
-        //X>=0
-        // X*num2 + X 2^i ==? num1
-        // X*num2 == num1 -  2^i-2^i2-2^i3
-        // X*num2 == num1 -  Y
-        // num2>0 num1 >0
-        // if x exist num2<num1 num2>0 && x*num2 = 2^j
-
-        // num2<0  num1 >0
-        // if  num1-2^j < num2
-        //
-        //
-        //  must find  2^i = X*num2
         if(num1==num2) return -1;
         if(num2>0){
             for(int i = 1;num1-num2*i>0;i++){
@@ -37,7 +22,6 @@ public class test0625b {
             // num2<0
             for(long i = 1;i<40;i++){
                 long f = (long)(num1*1L-num2*i*1L);
-//System.out.println("i: "+i +" f "+f);
                 if(Long.bitCount(f)<=i&&i<=f){
                     return (int)i;
                 }
