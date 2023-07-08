8644bc6d0df150a53c393f01f653f98d433d9ef7
Eric.Cui <465785032@qq.com>
2023-07-08 17:42:08 +0800

    twoSum3

--- src/main/java/org/cxz/algorithm/array/case167.java
+++ src/main/java/org/cxz/algorithm/array/case167.java
@@ -82,4 +82,19 @@ public class case167 {
         return -1;
 
     }
+    // TWO pointer
+    public int[] twoSum3(int[] numbers, int target) {
+        int l=0,r = numbers.length-1;
+        while(l<r){
+            int x = numbers[l]+numbers[r];
+            if(x== target){
+                return new int[]{l+1,r+1};
+            }
+            if(x>target) r--;
+            else l++;
+        }
+        return new int[0];
+    }
+
+
 }
