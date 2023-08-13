48785c19c7a56304289107236fe67c6ccb271d2e
Eric.Cui <465785032@qq.com>
2023-08-13 20:39:59 +0800

    another way

--- src/main/java/org/cxz/algorithm/array/case88.java
+++ src/main/java/org/cxz/algorithm/array/case88.java
@@ -21,4 +21,25 @@ public class case88 {
         }
         return;
     }
+
+    public void merge2(int[] nums1, int m, int[] nums2, int n) {
+        int p1 = m-1,p2 = n-1,idx = m+n-1;
+        while( idx>=0 ){
+            if(p1>=0&&p2>=0){
+                if(nums2[p2]>=nums1[p1]){
+                    nums1[idx--] = nums2[p2--];
+                }
+                else{
+                    nums1[idx--] = nums1[p1--];
+                }
+            }
+            else if(p1>=0){
+                nums1[idx--] = nums1[p1--];
+            }
+            else{
+                nums1[idx--] = nums2[p2--];
+            }
+        }
+        return;
+    }
 }
