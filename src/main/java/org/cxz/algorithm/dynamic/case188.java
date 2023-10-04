ccb5ff0a4b9c11e752bee5297cfc5a7d6f01d5d8
Eric.Cui <465785032@qq.com>
2023-10-04 11:29:04 +0800

    maxProfit2

--- src/main/java/org/cxz/algorithm/dynamic/case188.java
+++ src/main/java/org/cxz/algorithm/dynamic/case188.java
@@ -35,4 +35,34 @@ sell[i][j] = Math.max(sell[i - 1][j], buy[i - 1][j] + prices[i-1]);
         }
         return ans;
     }
+
+    int INF = Integer.MIN_VALUE/2;
+    public int maxProfit2(int k, int[] prices) {
+        int n = prices.length;
+        int[][][] f = new int[n+1][k+1][2];
+        //1 stock 0 money
+        //f[0][0][1] = INF;
+        // f[x][0][1] =inf f[x][0][0] = 0
+        //f[1][1][1] = -prices[0];
+        //f[1][1][0] = 0;
+        //f[i][j][0] = max(f[i][j][0],  max(   f[i-1][j][0]   ,   f[i-1][j][1]+p[i-1]) );
+        //f[i][j][1] = max(  f[i-1][j][1]    ,   f[i][j-1][0]-p[i-1] ) ;
+        for(int j = 0;j<=k;j++){
+            f[0][j][1]=INF;
+        }
+        //f[1][1][1] = -prices[0];
+        //(1,1,1)====  (0,1,1 inf) (1,0,0)-p === -p0
+        //(1,1,0)==== (0,1,0 : 0 )  ( 0,1,1 inf)-p === max(0,inf-p)
+        for(int i = 1;i<=n;i++){
+            int p = prices[i-1];
+            f[i][0][1] = INF;
+            for(int j = 1;j<=k;j++){
+                int t = Math.max(f[i-1][j][1],f[i][j-1][0]-p);
+                f[i][j][1] = t;
+                t = Math.max(f[i-1][j][0],f[i-1][j][1]+p);
+                f[i][j][0] = Math.max(f[i][j][0],t);
+            }
+        }
+        return f[n][k][0];
+    }
 }
