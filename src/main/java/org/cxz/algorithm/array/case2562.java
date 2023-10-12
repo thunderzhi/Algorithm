6e7ecb4efa26faa8887d8654dd7c3857f6079569
Eric.Cui <465785032@qq.com>
2023-10-12 09:40:05 +0800

    findTheArrayConcVal

---
+++ src/main/java/org/cxz/algorithm/array/case2562.java
@@ -0,0 +1,31 @@
+package org.cxz.algorithm.array;
+
+/**
+ * @author cxz
+ * @Title:
+ * @Package
+ * @Description:
+ * @date 2023/10/12 9:39
+ */
+public class case2562 {
+    public long findTheArrayConcVal(int[] nums) {
+        long ans = 0;
+        int i = 0;
+        int j = nums.length - 1;
+        while (i < j) {
+            int x = nums[i];
+            int y = nums[j];
+            while (y != 0) {
+                x *= 10;
+                y /= 10;
+            }
+            ans += x + nums[j];
+            i++;
+            j--;
+        }
+        if (i == j) {
+            ans += nums[i];
+        }
+        return ans;
+    } 
+}
